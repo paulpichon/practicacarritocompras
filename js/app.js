5f9606c6835577bccdabab8ecad8185333f034ce
Paul Pichon F <paul10_barca@hotmail.com>
2022-12-02 20:07:29 -0600

    Eliminar curso

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
 const listaCursos = document.querySelector('#lista-cursos');
 //variable donde se renderizaran los cursos en el carrito 
 const listaCarrito = document.querySelector('#lista-carrito tbody');
+//variable para poder borrar cursos
+const carrito = document.querySelector('#carrito');
+//vaciar carrito
+const vaciarCarrito = document.querySelector('#vaciar-carrito');
 //arreglo que ira alamacenado los curso seleccionados
 let cursosSeleccionados = [];
 
@@ -13,6 +17,17 @@ cargarEventListeners();
 function cargarEventListeners() {
     //listener a listacursos
     listaCursos.addEventListener('click', agregarCurso);
+    //listener a carrito para borrar cursos
+    carrito.addEventListener('click', borrarCurso);
+    //cargar los cursos de storage
+    document.addEventListener('DOMContentLoaded', () => {
+        //cargar cursos
+        cursosSeleccionados = JSON.parse(localStorage.getItem('carrito')) || [];
+        //llamamos funcion para renderizar
+        carritoHTML();
+    })
+    //vaciar carrito
+    vaciarCarrito.addEventListener();
 }
 
 //funciones
@@ -78,14 +93,37 @@ function carritoHTML() {
             <td>${titulo}</td>
             <td>${precio}</td>
             <td>${cantidad}</td>
-            <td><a href="#" class="borrar-curso" data-${id} >X</a></td>
+            <td><a href="#" class="borrar-curso" data-id=${id} >X</a></td>
         `;
         //renderizar
         listaCarrito.appendChild( row );
 
     });
     //sincronizar el localstorage
+    sincronizarStorage();
 }
+//funcion para eliminar un curso
+function borrarCurso( e ) {
+    //detectar cuando hagan click en borrar curso
+    if ( e.target.classList.contains('borrar-curso') ) {
+        //detectar cuando presionene "borrar-curso"
+        if (e.target.classList.contains('borrar-curso')) {
+            //id del curso
+            const idCurso = e.target.getAttribute('data-id');
+            //filter() para eliminar a curso seleccionado
+            cursosSeleccionados = cursosSeleccionados.filter( curso => curso.id !== idCurso );
+            //renderizamos
+            carritoHTML();
+
+        }
+    }
+}
+
+//funcion para sincronizar storage
+function sincronizarStorage() {
+    localStorage.setItem('carrito', JSON.stringify(cursosSeleccionados));
+}
+
 //funcion para limpiar el html
 function limpiarHTML() {
     while( listaCarrito.firstChild ){
